bce627bf2a2809b8ee6ce7d6525f115b079f3d30
Mario Rodriguez <Mario_Rodriguez_36@progressive.com>
2024-07-09 09:07:24 -0400

    End of First Day Push

---
+++ src/CatalogSolution/Catalog.Api/Catalog/ApiExtensions.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+
+namespace Catalog.Api.Catalog;
+
+public static class ApiExtensions
+{
+    public static IEndpointRouteBuilder MapCatalog(this IEndpointRouteBuilder routes)
+    {
+        var group = routes.MapGroup("/catalg");
+
+        group.MapPost("/{vendor}/{application}", AddItemAsync);
+
+        return routes;
+    }
+
+    public static async Task<Created<CatalogItemResponse>> AddItemAsync(CreateCatalogItemRequest request,
+        string vendor,
+        string application,
+        CancellationToken token)
+    {
+        var response = new CatalogItemResponse()
+        {
+            AnnualCostPerSeat = request.AnnualCostPerSeat,
+            Application = application,
+            Vendor = vendor,
+            Version = request.Version
+        };
+
+        return TypedResults.Created("slime", response);
+    }
+}
+
+public record CreateCatalogItemRequest
+{
+    public string Version { get; set; } = string.Empty;
+    public bool IsCommercial { get; set; }
+    public decimal AnnualCostPerSeat { get; set; }
+}
+
+public record CatalogItemResponse
+{
+
+    public string Vendor { get; set; } = string.Empty;
+    public string Application { get; set; } = string.Empty;
+    public string Version { get; set; } = string.Empty;
+    public decimal? AnnualCostPerSeat { get; set; }
+}
+
+/*- Vendor
+- Application
+- Version
+- Commercial or FOSS
+    - If it is commercial, the annual projected cost per seat.
+    - The Sub of the SoftwareControl person that added it, and the date and time it was added. */
---
+++ src/CatalogSolution/Catalog.Tests/Catalog/AddingToTheCatalog.cs
@@ -0,0 +1,39 @@
+﻿using Alba;
+using Catalog.Api.Catalog;
+
+namespace Catalog.Tests.Catalog;
+
+public class AddingToTheCatalog
+{
+    [Fact]
+    public async Task DoIt()
+    {
+        using var host = await AlbaHost.For<global::Program>();
+
+        var newCatalogItem = new CreateCatalogItemRequest
+        {
+            Version = "1.91",
+            IsCommercial = false,
+            AnnualCostPerSeat = 2.99M
+        };
+
+        var expectedResponse = new CatalogItemResponse
+        {
+            Vendor = "Microsoft",
+            Application = "VSCode",
+            AnnualCostPerSeat = 2.99M,
+            Version = "1.91"
+        };
+
+        var response = await host.Scenario(api =>
+        {
+            api.Post.Json(newCatalogItem).ToUrl("/catalog/microsoft/vscode");
+            api.StatusCodeShouldBe(201);
+        });
+
+        var body = await response.ReadAsJsonAsync<CatalogItemResponse>();
+
+        Assert.NotNull(body);
+        Assert.Equal(expectedResponse, body);
+    }
+}
